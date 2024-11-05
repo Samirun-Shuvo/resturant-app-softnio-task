4659c7ec669204a55ab39445d6d5f2e6fbda7cb8
Samirun-Shuvo <samirunshuvo@gmail.com>
2024-11-05 09:20:23 +0600

    booking form update

--- src/components/BookTable.jsx
+++ src/components/BookTable.jsx
@@ -11,29 +11,30 @@ const BookTable = () => {
         backgroundImage: `url(${bg})`,
         backgroundSize: "cover",
         backgroundPosition: "center",
+        objectFit: "center",
       }}
     >
       <section className="container my-5 py-5">
-        <div className="text-start mb-4">
-          <p className="text-muted fw-semibold d-flex align-items-center mb-2">
+        <div className="text-start my-2 py-2">
+          <p className="text-muted fw-semibold d-flex align-items-center my-2">
             <i className="bi bi-square-fill text-danger me-2"></i>
             <span className="text-danger">Book Now</span>
           </p>
-          <h1 className="display-6 fw-bold text-white text-uppercase">
+          <h1 className="display-6 fw-bold text-white text-uppercase my-2">
             Book Your Table
           </h1>
-          <small className="text-white">
+          <small className="text-white my-2">
             Enim tempor eget pharetra facilisis sed maecenas adipiscing. Eu leo
             molestie vel, ornare non id blandit netus.
           </small>
         </div>
-        <form className="row justify-content-start">
+        <form className="row justify-content-start my-5">
           <div className="col-12 col-md-6">
             <div className="row mb-3">
               <div className="col-12 col-md-6 mb-3 mb-md-0">
                 <input
                   type="text"
-                  placeholder="Your Name"
+                  placeholder="Your Name*"
                   className="text-white placeholder-white bg-transparent px-3 py-1 border border-1 w-100"
                 />
               </div>
@@ -49,8 +50,17 @@ const BookTable = () => {
               <div className="col-12 col-md-6">
                 <input
                   type="text"
+                  id="dateInput"
+                  className="date-placeholder text-white placeholder-white bg-transparent px-3 py-1 border border-1 w-100"
+                  onFocus={(e) => {
+                    e.target.type = "date";
+                    e.target.classList.add("date-input-focused");
+                  }}
+                  onBlur={(e) => {
+                    e.target.type = "text";
+                    e.target.classList.remove("date-input-focused");
+                  }}
                   placeholder="Reservation Date"
-                  className="text-white placeholder-white bg-transparent px-3 py-1 border border-1 w-100"
                 />
               </div>
               <div className="col-12 col-md-6">
@@ -58,6 +68,8 @@ const BookTable = () => {
                   type="text"
                   placeholder="Total People"
                   className="text-white placeholder-white bg-transparent px-3 py-1 border border-1 w-100"
+                  onFocus={(e) => (e.target.type = "number")}
+                  onBlur={(e) => (e.target.type = "text")}
                 />
               </div>
             </div>
